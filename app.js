582a94c7d91f1b3cc254ea16b09e791a8b60fa33
Siddhartha Paul <siddharthapaul@live.com>
2019-09-28 03:30:47 +0530

    sample ajax work explained

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
-const express= require("express");
-const path= require('path');
-const fs= require('fs');
-const session=require("express-session");
+const express = require("express");
+const path = require('path');
+const fs = require('fs');
+const session = require("express-session");
 const authRoutes = require('./routes/auth-routes');
 const passport = require('./config/passport-setup');
 // const mongoose = require('mongoose');
 // const keys = require('./config/keys');
-const app=express();
+const app = express();
 
 app.use(passport.initialize());
 app.use(session({
@@ -14,7 +14,7 @@ app.use(session({
     resave: false,
     saveUninitialized: true,
     cookie: { secure: false }
-  }))
+}))
 // // connect to mongoose
 // mongoose.connect(keys.mongodb.dbURI, (err) => {
 //     if(err){
@@ -29,12 +29,21 @@ app.use(express.static('public'));
 app.use('/auth', authRoutes);
 
 
-app.get('/', (req,res)=>{
+app.get('/', (req, res) => {
     // res.render('index.html');
-    res.sendFile('index.html',{root: path.join(__dirname, '/views')});
+    res.sendFile('index.html', { root: path.join(__dirname, '/views') });
 });
 
 
-app.listen(process.env.PORT || 5000,()=>{
+app.get('/userinfo', (req, res) => {
+    if (req.session.user) {
+        res.writeHead(200, {
+            'Content-Type': 'application/json'
+        });
+        res.end(JSON.stringify(req.session.user));
+    }
+});
+
+app.listen(process.env.PORT || 5000, () => {
     console.log(' app listening on port 5000');
 });
