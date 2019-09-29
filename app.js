14d571a5e0793f4ee73ee38a565df8a895080536
Jyoti Mishra <jyotimishra29091997@gmail.com>
2019-09-29 17:13:47 +0530

    post method used

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const path = require('path');
 const fs = require('fs');
+const bp=require("body-parser");
 const session = require("express-session");
 const authRoutes = require('./routes/auth-routes');
 const passport = require('./config/passport-setup');
-// const mongoose = require('mongoose');
-// const keys = require('./config/keys');
+const mongoose = require('mongoose');
+const keys = require('./config/keys');
+const User=require('/models/user-model')
 const app = express();
 
 app.use(passport.initialize());
@@ -15,25 +17,29 @@ app.use(session({
     saveUninitialized: true,
     cookie: { secure: false }
 }))
-// // connect to mongoose
-// mongoose.connect(keys.mongodb.dbURI, (err) => {
-//     if(err){
-//         console.log(err);
-//     }
-//     console.log('connected to mongoose');
-// });
+// connect to mongoose
+mongoose.connect(keys.mongodb.dbURI, (err) => {
+    if(err){
+        console.log(err);
+    }
+    console.log('connected to mongoose');
+});
 
 app.use(express.static('public'));
 
 //set up routes
 app.use('/auth', authRoutes);
 
+app.use(bp.json());
+
+app.use(bp.urlencoded({ 
+    extended: true
+})); 
 
 app.get('/', (req, res) => {
     // res.render('index.html');
     res.sendFile('index.html', { root: path.join(__dirname, '/views') });
 });
-
 
 app.get('/userinfo', (req, res) => {
     if (req.session.user) {
@@ -43,6 +49,18 @@ app.get('/userinfo', (req, res) => {
         res.end(JSON.stringify(req.session.user));
     }
 });
+
+app.get('/uservaluePost', (req, res) => {
+    mongoose.model('User').findOne('').then((data) => {({
+        thumbnail: req.body.usrProfile.src ,
+        username : req.body.name,
+        level : req.body.level,
+        score : req.body.score,
+        speed : req.body.speed,
+      }).save()
+      res.send()
+});
+
 
 app.listen(process.env.PORT || 5000, () => {
     console.log(' app listening on port 5000');
--- public/js/gameStructure.js
+++ public/js/gameStructure.js
@@ -4,16 +4,30 @@ class root{
     //set properties of svg
     //properties of root are height, width, fill, stroke parent conatiner
   }
+  isThereCollision(){
+
+  }
 
 }
 class paddle extends root{
   constructor(){
     super();
   }
+  movePaddle(offset)
+  {
+
+  }
+
 }
 class ball extends root{
   constructor(){
     super();
+  }
+  moveBall(){
+    if(this.isThereCollision())
+    {
+      
+    }
   }
 
 }
